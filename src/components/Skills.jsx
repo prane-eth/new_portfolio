57f02d8750592d595a6fdb9f36dab4c7b9d4ba1f
Praneeth <vhpraneeth2001@gmail.com>
2023-12-16 17:09:44 +0530

    Updated code

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,25 +2,24 @@ import { FaReact, FaNodeJs, FaPython, FaEthereum } from 'react-icons/fa'
 import { TbBrandJavascript } from 'react-icons/tb'
 import { SiDjango, SiMongodb, SiSolidity } from 'react-icons/si'
 
+const technologies = [
+	{ Icon: FaReact, color: "#61dbfb", name: "React" },
+	{ Icon: SiSolidity, color: '#002fa7', darkColor: 'blue', name: "Solidity" },
+	{ Icon: FaEthereum, color: "#3c3c3d", darkColor: '#ecf0f1', name: "Ethereum (Web3)" },
+	{ Icon: FaNodeJs, color: "#339933", darkColor: '#66cc33', name: "Node.js" },
+	{ Icon: TbBrandJavascript, color: "#f0db4f", name: "JavaScript" },
+	{ Icon: SiMongodb, color: "#4db33d", name: "MongoDB" },
+	{ Icon: FaPython, color: "#ffd43b", name: "Python" },
+	{ Icon: SiDjango, color: "#092e20", name: "Django" },
+]
+
 const Skills = ({ darkMode }) => (
 	<div id="Skills" className="skills-container-grid">
-		<span> <FaReact color="#61dbfb" className='icon-large' /> React </span>
-		<span>
-			<SiSolidity color={darkMode ? 'blue' : '#002fa7'} className='icon-large' />
-			Solidity
-		</span>
-		<span>
-			<FaEthereum color={darkMode ? '#ecf0f1' : "#3c3c3d"} className='icon-large' />
-			Ethereum (Web3)
-		</span>
-		<span>
-			<FaNodeJs color={darkMode ? '#66cc33' : "#339933"} className='icon-large' />
-			Node.js
-		</span>
-		<span> <TbBrandJavascript color="#f0db4f" className='icon-large' /> JavaScript </span>
-		<span> <SiMongodb color="#4db33d" className='icon-large' /> MongoDB </span>
-		<span> <FaPython color="#ffd43b" className='icon-large' /> Python </span>
-		<span> <SiDjango color="#092e20" className='icon-large' /> Django </span>
+		{technologies.map(({ Icon, color, darkColor, name }) => (
+			<span key={name}>
+				<Icon color={darkMode ? darkColor : color} className='icon-large' /> {name}
+			</span>
+		))}
 	</div>
 )
 
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,5 @@
 
-export const username = "vh-praneeth"
-export const linktree_username = "vh-praneeth"
+export const username = "prane-eth";
+export const linktree_username = "prane.eth";
 export const web3_repo = "web3_projects"
 export const githubUrl = `https://github.com/${username}`
